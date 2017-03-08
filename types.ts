430c640dc711ca3d5338a2b74bb386343c0987a1
mageshwaran1682 <mageshwaran1682@gmail.com>
2017-03-08 07:05:15 +0530

    TS basic types are added.

---
+++ types.ts
@@ -0,0 +1,36 @@
+// BOOLEAN
+let IsDone : boolean = false;
+
+// NUMBER
+let myAge : number = 35;
+
+// TEXT & STRING
+let myName : string = "Mageshwaran V";
+
+
+// TEMPLATE STRING
+let greeting : string = "Hello" + myName;
+let greeting2 : string = `Hello, ${myName}`;
+
+// ARRAYS
+let count : Array<number> = [1, 2, 3, 4];
+
+// If you don't know the type (watch out for using It's easy to use this a lot, but you'll miss out on type checking.)
+let anything : any = 5;
+anything = "we change to string";
+anything = false;
+
+// RETURN NOTHING
+function greeter(name: string) : void{
+    console.log("Hello" + myName);
+}
+
+// ENUMS
+
+let initialPos = 1;
+enum joystickDirections {up, down, right, left};
+
+if (initialPos === joystickDirections.down){
+    console.log (joystickDirections.down);
+}
+
